ae5dc821c5a315b4e4a0fd36b7cf9472fbfa6db0
ES103 <w624207158@gmail.com>
2022-05-01 22:03:36 -0500

    hw2

---
+++ enshi_wang_assignment2.js
@@ -0,0 +1,43 @@
+
+const itemsObject = [
+    {quantity: 1, price: 200},
+    {quantity: 3, price: 350},
+    {quantity: 5, price: 400},
+];
+
+//Q1
+const double = (obj) => {
+    newObj = {};
+    newObj.quantity = obj.quantity*2;
+    newObj.price =  obj.price*2;
+    return newObj;
+};
+let newArr =itemsObject.map(double);
+console.log(newArr);
+
+// Q2
+function fil(obj){
+    if (obj.quantity >2 & obj.price >300)
+     return true;
+    else
+     return false;
+}
+let res2 = itemsObject.filter(fil);
+console.log(res2);
+
+//Q3
+const sum = itemsObject.reduce((accumulator, object) => {
+    return accumulator + object.price;
+  }, 0);
+  
+  console.log(sum);
+
+//Q4
+const string =
+' Perhaps The Easiest-to-understand Case For Reduce Is To Return The Sum Of All The Elements In An Array ';
+
+function newstr(str) {
+	return str.replace(/[^a-zA-Z]/g,"").toLowerCase()};
+
+    console.log(newstr(string));
+
